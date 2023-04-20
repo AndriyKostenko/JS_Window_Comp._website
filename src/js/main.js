72f1cd34d54dde62ca6ff45a6616f2d9b8871d9a
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-04-20 19:37:19 -0400

    modals functionality completed

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import './slider';
+import modals from './modules/modals';
+
+
+//scripts will start only when DOM is fully ready
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+})
