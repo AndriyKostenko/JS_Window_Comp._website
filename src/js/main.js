5dbcacc16922ac3aca31f12c515540c7680857da
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-04-20 20:23:15 -0400

    added functionality for tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import './slider';
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 
 //scripts will start only when DOM is fully ready
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
 })
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,44 @@
+
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    // watching for whole area with tabs
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        //checking if click appeared exactly on tab
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }//checking if clicked tab inside of selected tabs area
+            });
+        }
+    });
+};
+
+export default tabs;
+
+
